80472d210012a81615a34b9ec349205e301b895f
Xikmatulloh <hikmatmahmadjonov20060826.@gmail.com>
2025-08-28 19:36:59 +0300

    add email verification

--- core/settings.py
+++ core/settings.py
@@ -150,6 +150,18 @@ STATICFILES_DIRS = ["static"]
 MEDIA_URL = "media/"
 MEDIA_ROOT = "media"
 
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/1"
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
